d7ef75a9eadfea657171c897d353658a891643b6
SihanSun <sis070@ucsd.edu>
2018-10-12 23:50:15 -0700

    event class

---
+++ src/main/java/com/SDHack/EventsClass/Events.java
@@ -0,0 +1,46 @@
+package com.SDHack.EventsClass;
+
+public class Events {
+
+    private int category;
+
+    private String name;
+
+    private int cost;
+
+    private String location;
+
+    private String startTime;
+
+    private String endTime;
+
+    private String pictureUri;
+
+    public int getCategory() { return category; }
+
+    public void setCategory(int category) { this.category = category; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public int getCost() { return cost; }
+
+    public void setCost(int cost) { this.cost = cost; }
+
+    public String getLocation() { return location; }
+
+    public void setLocation(String location) { this.location = location; }
+
+    public String getStartTime() { return startTime; }
+
+    public void setStartTime(String startTime) { this.startTime = startTime; }
+
+    public String getEndTime() { return endTime; }
+
+    public void setEndTime(String endTime) { this.endTime = endTime; }
+
+    public String getPictureUri() { return pictureUri; }
+
+    public void setPictureUri(String pictureUri) { this.pictureUri = pictureUri; }
+}
